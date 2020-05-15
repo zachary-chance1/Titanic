eab48778e999da50fdb243c7e4d98f9080ff3a36
zachary-chance1 <Zachary_Chance1@baylor.edu>
2020-05-15 01:21:58 -0500

    Completed 7a, 7b

--- R/titanic.R
+++ R/titanic.R
@@ -1,3 +1,11 @@
+################################################################
+#name: titanic.R
+#author: zachary chance (baylor university)
+#description: completes assignment 2 and performs analysis
+#             on the titanic data set
+#date: may 15, 2020
+#################################################################
+
 WORK.DIR = "C:/Users/Owner/Desktop/Titanic"
 
 # set working directory
@@ -11,3 +19,13 @@ if(any(not_installed)) install.packages(packages[not_installed])
 lapply(packages, require, character.only=TRUE)
 
 titanicdata = read.dta13("Data/titanic.dta")
+titanicdata$genderIsFemale = 0
+
+#7b - dummy for women
+len = dim(titanicdata)
+len = len[1]
+for(i in seq(1:len)){
+  if(titanicdata$sex[i] == "women"){
+    titanicdata$genderIsFemale[i] = 1
+  }
+}
